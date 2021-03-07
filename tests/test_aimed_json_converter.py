ff8b25624e735917af4e4f627fba681c7927a61c
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-03-06 22:19:08 -0800

    Add more tests to test multiple documents

--- tests/test_aimed_json_converter.py
+++ tests/test_aimed_json_converter.py
@@ -215,6 +215,68 @@ class TestAIMedJsonConverter(TestCase):
         # Assert
         self.assertEqual(expected_json, actual)
 
+    def test_convert_fromstring_multiple_documents(self):
+        """
+        Test case with multiple documents
+        :return:
+        """
+        # Arrange
+        data = """<corpus source="AIMed">
+            <document id="AIMed.d29">
+                <sentence id="AIMed.d29.s244" text="This work shows that single and double Ala substitutions of His18 and Phe21 in IL-8 reduced up to 77-fold the binding affinity to IL-8 receptor subtypes A (CXCR1) and B (CXCR2) and to the Duffy antigen." seqId="s244">
+                  <entity id="AIMed.d29.s244.e1" charOffset="130-133" type="protein" text="IL-8" seqId="e364"/>
+                  <entity id="AIMed.d29.s244.e5" charOffset="188-200" type="protein" text="Duffy antigen" seqId="e373"/>
+                  <interaction id="AIMed.d29.s244.i0" e1="AIMed.d29.s244.e1" e2="AIMed.d29.s244.e5" type="None" directed="false" seqId="i3"/>
+                </sentence>
+            </document>
+            <document id="AIMed.d28">
+                <sentence id="AIMed.d28.s234" text="We have identified a new TNF-related ligand, designated human GITR ligand (hGITRL), and its human receptor (hGITR), an ortholog of the recently discovered murine glucocorticoid-induced TNFR-related (mGITR) protein [4]." seqId="s234">
+                  <entity id="AIMed.d28.s234.e0" charOffset="62-65" type="protein" text="GITR" seqId="e329"/>
+                  <entity id="AIMed.d28.s234.e1" charOffset="62-72" type="protein" text="GITR ligand" seqId="e330"/>
+                  <interaction id="AIMed.d28.s234.i0" e1="AIMed.d28.s234.e0" e2="AIMed.d28.s234.e1" type="None" directed="false" seqId="i0"/>
+                </sentence>
+            </document>
+            </corpus>
+        """
+
+        expected_json = [
+            {"interacts": True
+                , "participant1Id": "AIMed.d29.s244.e1"
+                , "participant1Offset": 130
+                , "participant1Len": 4
+                , "participant1Text": "IL-8"
+                , "participant2Id": "AIMed.d29.s244.e5"
+                , "participant2Offset": 188
+                , "participant2Len": 13
+                , "participant2Text": "Duffy antigen"
+                , "documentId": "AIMed.d29"
+                , "sentenceId": "AIMed.d29.s244"
+                , "otherEntities": []
+             },
+
+            {"interacts": True
+                , "participant1Id": "AIMed.d28.s234.e0"
+                , "participant1Offset": 62
+                , "participant1Len": 4
+                , "participant1Text": "GITR"
+                , "participant2Id": "AIMed.d28.s234.e1"
+                , "participant2Offset": 62
+                , "participant2Len": 11
+                , "participant2Text": "GITR ligand"
+                , "documentId": "AIMed.d28"
+                , "sentenceId": "AIMed.d28.s234"
+                , "otherEntities": []
+             }
+        ]
+
+        sut = AIMedJsonConverter()
+
+        # Act
+        actual = sut.convert_fromstring(data)
+
+        # Assert
+        self.assertEqual(expected_json, actual)
+
     def test_convert_fromfile(self):
         # Arrange
         input_xml_file = os.path.join(os.path.dirname(__file__), "sample_data", "aimed.xml")
