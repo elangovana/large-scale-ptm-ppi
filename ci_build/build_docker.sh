0b6a71d2c71bc4630c4f951a850d821b852caac3
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-05-01 09:26:55 -0700

    Add build docker

--- ci_build/build_docker.sh
+++ ci_build/build_docker.sh
@@ -1,20 +1,24 @@
 DOCKER_BASE_DIR=$1
 IMAGE_REPO=$2 # e.g. of image repo 111111.dkr.ecr.us-east-2.amazonaws.com/image
+PYTORCH_DOCKER_ACCOUNT=763104351884
+
 
 AWS_ACCOUNT_ID=`echo $IMAGE_REPO | cut -f 1 -d "."`
-
 ECR_REGION=`echo $IMAGE_REPO | cut -f 4 -d "."`
+PYTORCH_DOCKER_ACCOUNT_URL={PYTORCH_DOCKER_ACCOUNT}.dkr.ecr.${ECR_REGION}.amazonaws.com
 
 
-
-echo Building the Docker image $IMAGE_REPO ...
+echo Building the Docker image $IMAGE_REPO from base ${PYTORCH_DOCKER_ACCOUNT_URL}...
 
 
 ## TODO: Automate version tagging based on datetime for now, ideally should be tied to release tags
-LATEST_TAG=latest
-VERSION_TAG=$(date '+%Y%m%d%H%M')
+VERSION=$(date '+%Y%m%d%H%M')
+device=gpu
 
-docker build -t $IMAGE_REPO:$LATEST_TAG   -f docker/Dockerfile $DOCKER_BASE_DIR
+LATEST_TAG=$device-latest
+VERSION_TAG=$device-$VERSION
+
+docker build -t $IMAGE_REPO:$LATEST_TAG   -f docker/Dockerfile $DOCKER_BASE_DIR --build-arg device=$device --build-arg account_url=${PYTORCH_DOCKER_ACCOUNT_URL}
 docker tag $IMAGE_REPO:$LATEST_TAG $IMAGE_REPO:$VERSION_TAG
 
 # Log into ecr
