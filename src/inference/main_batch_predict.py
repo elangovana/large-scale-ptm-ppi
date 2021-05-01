f9b935a3691ee6edc4c37c2f856499815b780ce6
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-05-01 15:42:40 -0700

    Prediction - Write predictions to file

--- src/inference/main_batch_predict.py
+++ src/inference/main_batch_predict.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 import sys
+from pathlib import Path
 
 from dataset_builder import DatasetBuilder
 from inference.ensemble_predictor import EnsemblePredictor
@@ -16,16 +17,19 @@ class BatchPredict:
     def _logger(self):
         return logging.getLogger(__name__)
 
-    def predict_from_directory(self, datajson, base_artefacts_dir, output_dir, is_ensemble, numworkers=None, batch=32):
+    def predict_from_directory(self, datajson, base_artefacts_dir, output_dir, is_ensemble, numworkers=None, batch=32,
+                               additional_args=None):
         data_files = [datajson]
         if os.path.isdir(datajson):
             data_files = glob.glob("{}/*.json".format(datajson))
 
         for d in data_files:
             self._logger.info("Running inference on file {}".format(d))
-            self.predict_from_file(d, base_artefacts_dir, output_dir, is_ensemble)
+            self.predict_from_file(d, base_artefacts_dir, output_dir, is_ensemble, numworkers, batch)
 
-    def predict_from_file(self, datajson, base_artifacts_dir, output_dir, is_ensemble, numworkers=None, batch=32):
+    def predict_from_file(self, data_file, base_artifacts_dir, output_dir, is_ensemble, numworkers=None, batch=32,
+                          additional_args=None):
+        additional_args = additional_args or {}
 
         artifacts_directories = []
         if is_ensemble:
@@ -40,9 +44,13 @@ class BatchPredict:
         with open(output_config, "r") as f:
             train_args = json.load(f)
 
+        train_args = {**train_args, **additional_args}
+
+        print(train_args)
+
         # Dataset Builder
         model_factory_name = train_args["modelfactory"]
-        dataset_builder = DatasetBuilder(val_data=datajson, dataset_factory_name=train_args["datasetfactory"],
+        dataset_builder = DatasetBuilder(val_data=data_file, dataset_factory_name=train_args["datasetfactory"],
                                          tokenisor_factory_name=model_factory_name,
                                          num_workers=numworkers, batch_size=batch,
                                          addition_args_dict=train_args)
@@ -59,9 +67,37 @@ class BatchPredict:
 
             models.append(model)
 
-        result = EnsemblePredictor().predict(models, dataset_builder.get_val_dataloader())
+        predictions, confidence_tensor = EnsemblePredictor().predict(models,
+                                                                     dataset_builder.get_val_dataloader())
 
-        return result
+        output_file = "{}.json".format(os.path.join(output_dir, Path(data_file).name))
+        self._write_results_to_file(predictions, confidence_tensor, dataset_builder.get_label_mapper(), output_file)
+
+        return predictions, confidence_tensor
+
+    def _write_results_to_file(self, predictions_tensor, confidence_scores_tensor, label_mapper, output_file):
+        result = []
+        confidence_scores_tensor = confidence_scores_tensor.cpu().tolist()
+        predictions = predictions_tensor.cpu().tolist()
+
+        # Convert indices to labels
+        for p, scores in zip(predictions, confidence_scores_tensor):
+            label_mapped_confidence = {s: label_mapper.reverse_map(si) for si, s in enumerate(scores)}
+            label_mapped_predictions = label_mapper.reverse_map(p)
+            predicted_confidence = scores[p]
+
+            # Prepare results
+            r = {
+                "prediction": label_mapped_predictions,
+                "confidence": predicted_confidence
+            }
+            r = {**label_mapped_confidence, **r}
+
+            result.append(r)
+
+        # Write json to file
+        with open(output_file, "w") as f:
+            json.dump(result, f)
 
 
 if "__main__" == __name__:
@@ -79,13 +115,19 @@ if "__main__" == __name__:
     parser.add_argument("--numworkers", help="The number of workers to use", type=int, default=None)
     parser.add_argument("--batch", help="The batchsize", type=int, default=32)
     parser.add_argument("--ensemble", help="Set to 1 if ensemble model", type=int, default=0, choices={0, 1})
-    args = parser.parse_args()
+    args, additional_args = parser.parse_known_args()
 
     print(args.__dict__)
+
+    # Convert additional args into dict
+    additional_dict = {}
+    for i in range(0, len(additional_args), 2):
+        additional_dict[additional_args[i].lstrip("--")] = additional_args[i + 1]
+    print(additional_dict)
 
     # Set up logging
     logging.basicConfig(level=logging.getLevelName(args.log_level), handlers=[logging.StreamHandler(sys.stdout)],
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     BatchPredict().predict_from_directory(args.datajson, args.artefactsdir, args.outdir, args.ensemble, args.numworkers,
-                                          args.batch)
+                                          args.batch, additional_dict)
