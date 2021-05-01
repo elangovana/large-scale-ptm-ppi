9ca4460e9388ff2c93ee4ab19464f071e72a61fb
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-05-01 10:50:29 -0700

    Fix docker pytorch version

--- ci_build/build_docker.sh
+++ ci_build/build_docker.sh
@@ -1,17 +1,18 @@
 DOCKER_BASE_DIR=$1
 IMAGE_REPO=$2 # e.g. of image repo 111111.dkr.ecr.us-east-2.amazonaws.com/image
 # Note: The account needs to match the pytorch release images, see https://github.com/aws/deep-learning-containers/blob/master/available_images.md
-PYTORCH_DOCKER_ACCOUNT=763104351884
+PYTORCH_REPO=$3
 
-AWS_ACCOUNT_ID=`echo $IMAGE_REPO | cut -f 1 -d "."`
 ECR_REGION=`echo $IMAGE_REPO | cut -f 4 -d "."`
-PYTORCH_DOCKER_ACCOUNT_URL=${PYTORCH_DOCKER_ACCOUNT}.dkr.ecr.${ECR_REGION}.amazonaws.com
+IMAGE_REPO_ACCOUNT=`echo $IMAGE_REPO | cut -f 1 -d "."`
+PYTORCH_DOCKER_ACCOUNT =`echo $PYTORCH_REPO | cut -f 1 -d "."`
 echo Building the Docker image $IMAGE_REPO from base ${PYTORCH_DOCKER_ACCOUNT_URL}...
 
 
 ## TODO: Automate version tagging based on datetime for now, ideally should be tied to release tags
 VERSION=$(date '+%Y%m%d%H%M')
 device=gpu
+cuda=101
 
 LATEST_TAG=$device-latest
 VERSION_TAG=$device-$VERSION
@@ -20,7 +21,7 @@ VERSION_TAG=$device-$VERSION
 # aws ecr get-login-password --region region | docker login --username AWS --password-stdin ${PYTORCH_DOCKER_ACCOUNT_URL}
 $(aws ecr get-login --no-include-email --region $ECR_REGION --registry-ids ${PYTORCH_DOCKER_ACCOUNT} )
 
-docker build -t $IMAGE_REPO:$LATEST_TAG   -f docker/Dockerfile $DOCKER_BASE_DIR --build-arg device=$device --build-arg account_url=${PYTORCH_DOCKER_ACCOUNT_URL}
+docker build -t $IMAGE_REPO:$LATEST_TAG   -f docker/Dockerfile $DOCKER_BASE_DIR --build-arg device=$device --build-arg account_url=${PYTORCH_REPO} --build-arg cuda=${cuda}
 docker tag $IMAGE_REPO:$LATEST_TAG $IMAGE_REPO:$VERSION_TAG
 
 # Log into ecr
