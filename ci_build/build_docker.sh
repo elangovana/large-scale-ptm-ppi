0d8e096bcd798a5e02831ad9a54074957ece8194
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-05-08 16:40:00 -0700

    Update docker

--- ci_build/build_docker.sh
+++ ci_build/build_docker.sh
@@ -3,7 +3,7 @@ IMAGE_REPO=$2 # e.g. of image repo 111111.dkr.ecr.us-east-2.amazonaws.com/image
 # Note: The account needs to match the pytorch release images, see https://github.com/aws/deep-learning-containers/blob/master/available_images.md
 # e.g.763104351884.dkr.ecr.us-east-2.amazonaws.com/pytorch-training
 PYTORCH_REPO=$3
-echo Running with arguments $@
+echo Running with arguments "$@"
 
 
 ECR_REGION=`echo $IMAGE_REPO | cut -f 4 -d "."`
@@ -22,7 +22,7 @@ function build_docker(){
   CUDA=$3
   LATEST_TAG=$4
 
-  echo build docker with args $@
+  echo build docker with args "$@"
 
   # Login to pytorch ECR before build
   # aws ecr get-login-password --region region | docker login --username AWS --password-stdin ${PYTORCH_DOCKER_ACCOUNT_URL}
@@ -31,12 +31,11 @@ function build_docker(){
   docker build -t $IMAGE_REPO:$TAG   -f docker/Dockerfile $DOCKER_BASE_DIR --build-arg device=$DEVICE --build-arg account_url=${PYTORCH_REPO} --build-arg cuda=${CUDA}
 
   echo Logging in to Amazon ECR...
-  $(aws ecr get-login --no-include-email --region $ECR_REGION)
+  $(aws ecr get-login --no-include-email --region $ECR_REGION --registry-ids ${IMAGE_REPO_ACCOUNT})
   echo Pushing the Docker image $IMAGE_REPO:$TAG...
   docker push $IMAGE_REPO:$TAG
 
-  if [-n $LATEST_TAG]
-  then
+  if [ -n "$LATEST_TAG" ]; then
     echo tagging the Docker image $IMAGE_REPO:$LATEST_TAG...
     docker tag $IMAGE_REPO:$TAG $IMAGE_REPO:$LATEST_TAG
     docker push $IMAGE_REPO:$LATEST_TAG
