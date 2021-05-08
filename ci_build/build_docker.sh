1c50b1211afea4a5266009fd707edd786d6f750c
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-05-08 16:17:47 -0700

    Update docker

--- ci_build/build_docker.sh
+++ ci_build/build_docker.sh
@@ -14,23 +14,41 @@ echo Building the Docker image $IMAGE_REPO from base ${PYTORCH_DOCKER_ACCOUNT_UR
 
 ## TODO: Automate version tagging based on datetime for now, ideally should be tied to release tags
 VERSION=$(date '+%Y%m%d%H%M')
+
+
+function build_docker{
+  DEVICE=$1
+  TAG=$2
+  CUDA=$3
+  LATEST_TAG=$4
+
+  # Login to pytorch ECR before build
+  # aws ecr get-login-password --region region | docker login --username AWS --password-stdin ${PYTORCH_DOCKER_ACCOUNT_URL}
+  $(aws ecr get-login --no-include-email --region $ECR_REGION --registry-ids ${PYTORCH_DOCKER_ACCOUNT} )
+
+  docker build -t $IMAGE_REPO:$TAG   -f docker/Dockerfile $DOCKER_BASE_DIR --build-arg device=$DEVICE --build-arg account_url=${PYTORCH_REPO} --build-arg cuda=${CUDA}
+
+  echo Logging in to Amazon ECR...
+  $(aws ecr get-login --no-include-email --region $ECR_REGION)
+  echo Pushing the Docker image...
+  docker push $IMAGE_REPO:$TAG
+
+  if [-n $LATEST_TAG]
+  then
+    docker tag $IMAGE_REPO:$LATEST_TAG $IMAGE_REPO:$TAG
+    docker push $IMAGE_REPO:$TAG
+
+  fi
+
+}
+
 device=gpu
 cuda=cu101
-
 LATEST_TAG=$device-latest
 VERSION_TAG=$device-$VERSION
+build_docker $device $VERSION_TAG $cuda $LATEST_TAG
 
-# Login to pytorch ECR before build
-# aws ecr get-login-password --region region | docker login --username AWS --password-stdin ${PYTORCH_DOCKER_ACCOUNT_URL}
-$(aws ecr get-login --no-include-email --region $ECR_REGION --registry-ids ${PYTORCH_DOCKER_ACCOUNT} )
-
-docker build -t $IMAGE_REPO:$LATEST_TAG   -f docker/Dockerfile $DOCKER_BASE_DIR --build-arg device=$device --build-arg account_url=${PYTORCH_REPO} --build-arg cuda=${cuda}
-docker tag $IMAGE_REPO:$LATEST_TAG $IMAGE_REPO:$VERSION_TAG
-
-# Log into ecr
-echo Logging in to Amazon ECR...
-$(aws ecr get-login --no-include-email --region $ECR_REGION)
-
-echo Pushing the Docker image...
-docker push $IMAGE_REPO:$LATEST_TAG
-docker push $IMAGE_REPO:$VERSION_TAG
+device=cpu
+LATEST_TAG=$device-latest
+VERSION_TAG=$device-$VERSION
+build_docker $device $VERSION_TAG
