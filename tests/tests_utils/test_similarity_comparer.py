Fix similarity computation

tests/tests_utils/test_similarity_comparer.py
@@ -27,7 +27,7 @@ class TestSimilarityComparer(TestCase):
         best_score, best_match = sut(train_data, test_data)
 
         # Assert
-        self.assertSequenceEqual(expected_score, best_score)
+        self.assertSequenceEqual(expected_score.round(2), best_score.round(2))
         self.assertSequenceEqual(best_match, expected_match)
 
     def test__call_two(self):
@@ -40,10 +40,10 @@ class TestSimilarityComparer(TestCase):
 
         test_data = [
             "This is test data",
-            "Nothing to do"
+            "Nothing training to do"
         ]
 
-        expected_score = np.array([1.0, 0.5])
+        expected_score = np.array([1.0, 0.71])
         expected_match = np.array(["This is test data", "Nothing is training data"])
 
         sut = SimilarityComparer(n_gram=1)
@@ -52,5 +52,5 @@ class TestSimilarityComparer(TestCase):
         best_score, best_match = sut(train_data, test_data)
 
         # Assert
-        self.assertSequenceEqual(expected_score.tolist(), best_score.tolist())
+        self.assertSequenceEqual(expected_score.round(2).tolist(), best_score.round(2).tolist())
         self.assertSequenceEqual(best_match.tolist(), expected_match.tolist())