8716ff88428b2a575411b81d5132ddf765151360
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-10-16 13:54:33 -0700

    Fix similarity computation

--- src/utils/top_words_similarity_comparer.py
+++ src/utils/top_words_similarity_comparer.py
@@ -3,17 +3,18 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-class SimilarityComparer:
+class TopWordsSimilarityComparer:
 
     def __init__(self, n_gram=1, stop_words='english'):
         self._vectorizer = CountVectorizer(analyzer='word', ngram_range=(n_gram, n_gram), stop_words=stop_words)
 
     def __call__(self, list_reference, list_new):
-        # Merge ref and new so no unknown words
         vec = self._vectorizer.fit(list_reference + list_new)
+        x_vector_ref = self._vectorizer.transform(list_reference)
+        y_vector_new = self._vectorizer.transform(list_new)
 
-        x_vector_ref = vec.transform(list_reference)
-        y_vector_new = vec.transform(list_new)
+        bag_of_words = vec.transform(list_reference)
+        print(x_vector_ref)
 
         sim_scores = cosine_similarity(y_vector_new, x_vector_ref, dense_output=False).toarray()
 
