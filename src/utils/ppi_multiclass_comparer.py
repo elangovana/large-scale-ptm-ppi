ff0548e2447ae81f9039d2b81dfb5313f8de5613
elangovana <d0b4bd2d24da37296a6db2b5d68031999355fc56>
2021-10-16 16:01:12 -0700

    Fix similarity computation

--- src/utils/ppi_multiclass_comparer.py
+++ src/utils/ppi_multiclass_comparer.py
@@ -10,9 +10,20 @@ class PpiMulticlassComparer:
     def compare(self, reference_file, new_file):
         dataset_factory = PpiMulticlassDatasetFactory()
         ref_data = dataset_factory.get_dataset(reference_file)
-        new_file = dataset_factory.get_dataset(new_file)
+        new_data = dataset_factory.get_dataset(new_file)
 
-        ref_data_list = [x for x, _ in ref_data]
-        new_data_list = [x for x, _ in new_file]
+        # Retain original labels without the index
+        ref_data.label_transformer = None
+        new_data.label_transformer = None
 
-        return self._comparer(ref_data_list, new_data_list)
+        ref_labels = list(set([y_label for _, y_label in ref_data]))
+
+        result = {}
+        for y_label in ref_labels:
+            ref_data_list = [x for x, y in ref_data if y == y_label]
+            new_data_list = [x for x, y in new_data if y == y_label]
+
+            if len(new_data_list) == 0: continue
+            result[y_label] = self._comparer(ref_data_list, new_data_list)
+
+        return result
