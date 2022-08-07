1ce3056bede9a8e906a6409cdcff39a56347ffc9
elangovana <aparnaelangovan@gmail.com>
2022-08-06 19:30:25 -0700

    Add option for large scale prediction

--- src/inference/chemprot_batch_predict.py
+++ src/inference/chemprot_batch_predict.py
@@ -11,7 +11,8 @@ from inference.batch_predict import BatchPredict
 
 class ChemprotBatchPredict:
 
-    def __init__(self, batch_predict, filter_std=0.15, use_filter=False):
+    def __init__(self, batch_predict, filter_std=0.15, use_filter=False, filter_threshold_negative=None):
+        self.filter_threshold_negative = filter_threshold_negative
         self.use_filter = use_filter
         self.filter_std = filter_std
         self.batch_predict = batch_predict
@@ -27,12 +28,40 @@ class ChemprotBatchPredict:
         with  tarfile.open(tar_gz_file) as tf:
             tf.extractall(dest_dir)
 
+    def predict_from_dir(self, datajson, base_artefacts_dir, is_ensemble, output_dir=None, numworkers=None,
+                         batch=32, additional_args=None, raw_data_reader_func=None, filter_func=None):
+        # if one file in artifacts dir then untar
+        self._logger.info(f"Checking if just one tar file exists in {base_artefacts_dir}")
+
+        if len(os.listdir(base_artefacts_dir)) == 1 and os.listdir(base_artefacts_dir)[0].endswith("tar.gz"):
+            model_dir = os.path.join(base_artefacts_dir, "model")
+            self._extract_tar(os.path.join(base_artefacts_dir, os.listdir(base_artefacts_dir)[0]), model_dir)
+            base_artefacts_dir = model_dir
+            self._logger.info(f"Setting base dir to {base_artefacts_dir}")
+
+        raw_data_reader_func = raw_data_reader_func or self._get_chemprot_inference_reader
+
+        # Use filter if
+        default_filter = lambda p, c, s: True
+        if self.use_filter:
+            self._logger.info("Using filter with threshold {}".format(self.filter_std))
+            default_filter = lambda p, c, s: (c > self.filter_threshold_negative and p == 'NEGATIVE') or (
+                    s <= self.filter_std)
+        filter_func = filter_func or default_filter
+
+        return list(self.batch_predict.predict_from_directory(datajson, base_artefacts_dir,
+                                                              is_ensemble, output_dir=output_dir,
+                                                              numworkers=numworkers, batch=batch,
+                                                              additional_args=additional_args,
+                                                              raw_data_reader_func=raw_data_reader_func,
+                                                              filter_func=filter_func))
+
     def predict_from_file(self, datajson, base_artefacts_dir, is_ensemble, output_file, numworkers=None, batch=32,
                           additional_args=None, raw_data_reader_func=None, filter_func=None):
         raw_data_reader_func = raw_data_reader_func or self._get_chemprot_inference_reader
 
         # if one file in artifacts dir then untar
-        self._logger.info(f"Checking if just one targ file exists in {base_artefacts_dir}")
+        self._logger.info(f"Checking if just one tar file exists in {base_artefacts_dir}")
         if len(os.listdir(base_artefacts_dir)) == 1 and os.listdir(base_artefacts_dir)[0].endswith("tar.gz"):
             model_dir = os.path.join(base_artefacts_dir, "model")
             self._extract_tar(os.path.join(base_artefacts_dir, os.listdir(base_artefacts_dir)[0]), model_dir)
@@ -50,10 +79,10 @@ class ChemprotBatchPredict:
 
 def parse_args_run():
     parser = argparse.ArgumentParser()
-    parser.add_argument("datajson",
-                        help="The json data to predict")
+    parser.add_argument("datadir",
+                        help="The json data dir to predict")
     parser.add_argument("artefactsdir", help="The base of artefacts dir that contains directories of model, vocab etc")
-    parser.add_argument("outfile", help="The output file")
+    parser.add_argument("outdir", help="The output dir")
     parser.add_argument("--log-level", help="Log level", default="INFO", choices={"INFO", "WARN", "DEBUG", "ERROR"})
     parser.add_argument("--numworkers", help="The number of workers to use", type=int, default=None)
     parser.add_argument("--batch", help="The batchsize", type=int, default=32)
@@ -74,13 +103,13 @@ def parse_args_run():
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     ChemprotBatchPredict(BatchPredict(), use_filter=args.filter,
-                         filter_std=args.filterstdthreshold).predict_from_file(args.datajson,
-                                                                               args.artefactsdir,
-                                                                               args.ensemble,
-                                                                               args.outfile,
-                                                                               args.numworkers,
-                                                                               args.batch,
-                                                                               additional_dict)
+                         filter_std=args.filterstdthreshold).predict_from_dir(args.datadir,
+                                                                              args.artefactsdir,
+                                                                              args.ensemble,
+                                                                              args.outdir,
+                                                                              args.numworkers,
+                                                                              args.batch,
+                                                                              additional_dict)
 
 
 if "__main__" == __name__:
